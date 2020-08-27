1e9717c5d10369978227c8456dee5fa3f2170c38
Ignacio Tampe <Igtampe@outlook.com>
2020-08-27 13:14:27 -0400

    Creation of console project
    Client no longer has BasicRender toolset as part of its references

---
+++ LBL.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBL.Console {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
